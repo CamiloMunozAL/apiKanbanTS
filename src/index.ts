c7e9a0e94171e8d38229531fcddc31d23e2a71d9
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-07-11 07:06:37 -0500

    cimit

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,21 @@ import authRouter from "./routes/authRouter";
 import sectionRouter from "./routes/sectionRouter";
 import taskRouter from "./routes/taskRouter";
 import userRouter from "./routes/userRouter";
+import cors from "cors";
 import { Request, Response } from "express";
+import { METHODS } from "http";
+
 console.clear();
+
+const corsOptions ={
+    origin: "*",
+    METHODS: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.get("/", (_, res) => {
     res.send("Kanban API");
