71b3f505ed597561a635e14211116a5f94c71baa
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-07-13 19:53:57 -0500

    Add html doc

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,27 @@ import sectionRouter from "./routes/sectionRouter";
 import taskRouter from "./routes/taskRouter";
 import userRouter from "./routes/userRouter";
 import cors from "cors";
+import path from "path";
 import { Request, Response } from "express";
-import { METHODS } from "http";
 
 console.clear();
 
 const corsOptions ={
     origin: "*",
-    METHODS: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.get("/", (_, res) => {
-    res.send("Kanban API");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.use("/api",authRouter);
@@ -30,10 +34,10 @@ app.use("/api",sectionRouter);
 app.use("/api",taskRouter);
 app.use("/api",userRouter);
 
-// // Middleware para manejar rutas no encontradas
-// app.use((_: Request, res: Response) => {
-//     res.status(404).send("Path not found");
-// });
+// Middleware para manejar rutas no encontradas
+app.use((_: Request, res: Response) => {
+    res.status(404).send("Path not found");
+});
 
 
 app.listen(PORT, () => {
